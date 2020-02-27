dd5ca4c77f6685eb52cd940ffd93979ecafb1e87
Eric Larson <EricDLarson@users.noreply.github.com>
2020-02-27 11:43:34 -0800

    Use POST for purge

--- curl/delete_events.sh
+++ curl/delete_events.sh
@@ -5,9 +5,15 @@
 # The filter field specifies a time range (eventTime) and/or eventType
 # Note the encoding for the filter parameter, and times are in "zulu" format.  In this case we've encoded:
 # filter=eventTime > "2000-04-23T18:25:43.511Z" eventTime < "2001-04-23T18:30:43.511Z"
+# force:true will cause an actual delete, force:false (the default) will return only return a list of events that match
 
 export GOOGLE_APPLICATION_CREDENTIALS=<yourkeyfile>
 export PROJECT=<yourprojectnumber>
 
-curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token)"" \
-     \"https://recommendationengine.googleapis.com/v1beta1/projects/$PROJECT/locations/global/catalogs/default_catalog/eventStores/default_event_store/userEvents:purge?filter=eventTime%20%3E%20%222000-04-23T18%3A25%3A43.511Z%22%20eventTime%20%3C%20%222001-04-23T18%3A30%3A43.511Z%22\"
+curl -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token)"" \
+      -H "Content-Type: application/json; charset=utf-8" \
+     --data '{
+              "filter":"eventTime > \"2019-12-23T18:25:43.511Z\" eventTime < \"2019-12-23T18:30:43.511Z\"",
+              "force":"true"
+    }' \
+    "https://recommendationengine.googleapis.com/v1beta1/projects/$PROJECT_NUM/locations/global/catalogs/default_catalog/eventStores/default_event_store/userEvents:purge"
