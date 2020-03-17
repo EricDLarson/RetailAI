80733687990e9648a75fcab5515d687c587a642f
Eric Larson <eric@ericdlarson.com>
2020-03-17 09:55:53 -0700

    python curl example

---
+++ python/curl_predict.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+# This sample constructs and executes a simple curl command to call the
+# predict API,
+
+import argparse
+import os
+import json
+
+APIKEY='<your registered API key>'
+PROJECT='<your project number>'
+PLACEMENT='<your placement id>'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('itemids',
+                    help='comma-seperated list of item ids')
+
+args = parser.parse_args()
+
+data = {
+    'filter': 'tag="safe"',
+    'dryRun': False,
+    'pageSize': 4,
+    'params': {
+        'returnCatalogItem': False,
+        'returnItemScore': True
+    },
+    'userEvent': {
+        'eventType': 'detail-page-view',
+        'userInfo': {
+            'visitorId': 'visitor1',
+            'userId': 'user1',
+            'ipAddress': '0.0.0.0',
+            'userAgent': 'Mozilla/5.0 (Windows NT 6.1)'
+        },
+      'productEventDetail': {
+        'productDetails': [
+        ]
+      }
+    }
+}
+
+for id in args.itemids.split(','):
+  data['userEvent']['productEventDetail']['productDetails'].append(
+    {'id': str(id)}
+  )
+
+
+CMD='curl -X POST -H "Content-Type: application/json; charset=utf-8" ' + \
+'--data  \'' + json.dumps(data) + \
+'\' https://recommendationengine.googleapis.com/v1beta1/projects/' + \
+PROJECT + \
+'/locations/global/catalogs/default_catalog/eventStores' + \
+'/default_event_store/placements/' + \
+PLACEMENT + ':predict?key=' + APIKEY
+
+print(CMD)
+os.system(CMD)
+
