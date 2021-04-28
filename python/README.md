c4415d50d8d17b42ebea685dbf299bec1a755fcf
Eric Larson <eric@ericdlarson.com>
2021-04-28 09:21:11 -0700

    python example

---
+++ python/README.md
@@ -0,0 +1,5 @@
+## Recommendations AI Python Examples using Retail API
+
+[Python Client Library](https://pypi.org/project/google-cloud-retail/)
+
+#### All samples provided as-is without warranty
---
+++ python/import_catalog_inline.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Inline import catalog items using Retail API
+
+"""
+
+from google.cloud import retail
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = "<path to service account json file>"
+PROJECT_NUM = "<project # or id>"
+
+SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+credentials = service_account.Credentials.from_service_account_file(
+  SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+client = retail.ProductServiceClient(credentials=credentials)
+
+products = {
+    "products": [
+      {
+        "id": "123",
+        "title": "Steamed Hams",
+        "description": "Bulk Lot of Steamed Hams",
+        "categories": ["Food","Steamed & Canned"],
+        "price_info": {
+          "price": 149.95,
+          "currency_code": "USD"
+        }
+      },
+      {
+        "id": "456",
+        "title": "Canned Yams",
+        "description": "Bulk Lot of Canned Yams",
+        "categories": ["Food","Steamed & Canned"],
+        "price_info": {
+          "price": 89.95,
+          "currency_code": "USD"
+        }
+      }
+    ]
+}
+  
+inputConfig = {"product_inline_source": products}
+parent = 'projects/' + PROJECT_NUM + '/locations/global/catalogs/default_catalog/branches/0'
+
+response = client.import_products(parent,inputConfig)
+
