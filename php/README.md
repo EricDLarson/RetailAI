2f132a6d95bcda409f521045d8d67ac18c818342
Eric <github@ericdlarson.com>
2021-04-29 15:05:22 -0700

    php predict examples using client libraries

---
+++ php/README.md
@@ -0,0 +1,2 @@
+Current php client libraries for google-cloud-retail are here:
+[https://github.com/googleapis/google-cloud-php/tree/master/Retail](https://github.com/googleapis/google-cloud-php/tree/master/Retail)
---
+++ php/predict.php
@@ -0,0 +1,51 @@
+<?php
+
+// Example using Retail php client libraries to get prediction results
+
+require 'vendor/autoload.php';
+
+use Google\Cloud\Retail\V2\PredictionServiceClient;
+use Google\Cloud\Retail\V2\PredictResponse;
+use Google\Cloud\Retail\V2\Product;
+use Google\Cloud\Retail\V2\ProductDetail;
+use Google\Cloud\Retail\V2\UserEvent;
+
+$predictionClient = new PredictionServiceClient([
+  'credentials' => '[your service account key.json file]'
+]);
+
+$placement = "projects/[YOUR PROJECT #]/locations/global/catalogs/default_catalog/placements/product_detail";
+
+$product_detail = new ProductDetail([
+  'product' => new Product(['id' => '12345']),
+  'quantity' => 1
+]);
+
+$userEvent = new UserEvent([
+  'event_type' => 'detail-page-view',
+  'visitor_id' => 'ABCDEFG',
+  'product_details' => [
+    new ProductDetail([
+      'product' => new Product(['id' => '12345']),
+      'quantity' => 1
+    ])
+  ]
+]);
+
+try {
+  $predictions = $predictionClient->predict($placement, $userEvent);
+} finally {
+  $predictionClient->close();
+}
+
+$results = $predictions->getResults();
+
+print($results->count() . " Item Id's Returned:");
+
+$iterator = $results->getIterator();
+while($iterator->valid()) {
+  print($iterator->current()->getId() . "\n");
+  $iterator->next();
+}
+
+?>
