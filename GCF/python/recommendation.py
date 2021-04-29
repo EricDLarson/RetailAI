01e06577b2f85b69dcfc699a827af2a08a8cc957
Eric <github@ericdlarson.com>
2021-04-28 18:11:12 -0700

    Updated GCF to use Retail API with service account (no more API key)

--- GCF/python/recommendation.py
+++ GCF/python/recommendation.py
@@ -1,67 +1,103 @@
-"""Cloud function for returning JSON via AJAX request (JSONP).
+""" Google Cloud Function to return JSON or HTML from predict response
 
-Simply deploy this as a cloud function with your correct API KEY and project
+This sample uses the Retail API with client libraries
+
+This can be used for doing AJAX/client side calls to get prediction results
+and render in a div.
+
+Configure the GCF to use a service account which has Retail Editor Role
+
 """
 
+import google.auth
 import json
-import requests
 
-API_KEY = '<yourpredictapikey>'
-PROJECT_NUMBER = '<yourprojectnumber>'
+from google.cloud import retail
+from google.oauth2 import service_account
 
+PROJECT_NUMBER='718209125704'
+
+credentials, project = google.auth.default(
+    scopes=[
+      'https://www.googleapis.com/auth/cloud-platform'
+    ]
+)
+
+client = retail.PredictionServiceClient(credentials=credentials)
 
 def recommend(request):
-  """Main function to be called.
-
-  Args:
-    request: Flask HTTP request.
-  Returns:
-    A string (JSONP function in this case)
-  """
 
   if request.args and 'visitorid' in request.args:
     visitorid = request.args.get('visitorid')
   else:
-    visitorid = ''
+    visitorid = ""
 
   if request.args and 'productid' in request.args:
     productid = request.args.get('productid')
   else:
-    productid = ''
+    productid = ""
 
   if request.args and 'num' in request.args:
-    page_size = request.args.get('num')
+    pageSize = request.args.get('num')
   else:
-    page_size = 4
+    pageSize = 4
 
-  payload = {
-      'filter': 'filterOutOfStockItems',
-      'pageSize': page_size,
-      'params': {
-          'returnCatalogItem': True
-      },
-      'userEvent': {
-          'eventType': 'detail-page-view',
-          'userInfo': {
-              'visitorId': visitorid,
-          },
-          'productEventDetail': {
-              'productDetails': [{
-                  'id': productid,
-                  'currencyCode': 'USD'
-              }]
-          }
+  user_event = {
+    'event_type': 'detail-page-view',
+    'visitor_id': visitorid,
+    'product_details': [{
+      'product': {
+        'id': productid,
       }
+    }]
   }
 
-  r = requests.post(('https://recommendationengine.googleapis.com/v1beta1/'
-                     'projects/' + PROJECT_NUMBER + '/locations/global/'
-                     'catalogs/default_catalog/'
-                     'eventStores/default_event_store/'
-                     'placements/product_detail:predict?key=' + API_KEY),
-                    data=json.dumps(payload))
+  # Set as necessary for your requirements & placement id:
+  predict_request = {
+    "placement":
+      'projects/' + PROJECT_NUMBER +
+      '/locations/global/catalogs/default_catalog/placements/product_detail',
 
-  # Return JSONP result
-  result = 'displayRecs(' + r.text + ')'
+    "user_event": user_event,
 
-  return result
+    "page_size": pageSize,
+
+    "filter": 'filterOutOfStockItems tag="safe"',
+
+    "params": {
+      "returnProduct": True,
+      "returnScore": True
+    }
+  }
+
+  response = client.predict(predict_request)
+
+  # Configure as necessary - here we just return a few fields
+  # that we need for rendering the results
+  items = []
+  for rec in response.results:
+    product = rec.metadata.get('product')
+    images = product.get('images')
+
+    item = {
+      "id": rec.id,
+      "title": product.get('title'),
+      "uri": product.get('uri'),
+      "img": images[0]['uri']
+    }
+
+    items.append(item)
+
+  # option to return full div or JSON
+  if request.args and 'html' in request.args:
+    result = '<div>'
+
+    for item in items:
+      result =(result + '<img src="' + item['uri'] + '">')
+
+    result = result + '</div>'
+
+  else:
+    result = 'displayRecs(' + json.dumps(items) + ')'
+
+  return (result)
